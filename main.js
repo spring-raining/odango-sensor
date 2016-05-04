75ec7e8339c38b69526aba38caa897afffd2ed9c
spring-raining <fdonthavei@gmail.com>
2016-05-04 16:02:59 +0900

    Scratch

---
+++ main.js
@@ -0,0 +1,34 @@
+const serialport                = require('serialport');
+const SerialPort                = serialport.SerialPort;
+
+const DEVICE_PATH               = require('./defs.json').device_path;
+const BAUDRATE                  = require('./defs.json').baudrate;
+const METRICS_DEFINITION        = require('./metrics.json');
+
+const sp = new SerialPort(DEVICE_PATH, {
+  baudrate: BAUDRATE,
+  parser: serialport.parsers.readline('\n'),
+}, false);
+
+sp.open((err) => {
+  if (err) {
+    return console.error('Error opening port: ', err.message);
+  }
+  sp.on('data', (data) => {
+    const parsed = /Humidity:(\S+)\tTemperature:(\S+)/.exec(data);
+
+    if (parsed) {
+      callback(parsed[1], parsed[2]);
+      sp.close();
+    }
+  });
+});
+
+function callback(humidity, temperature) {
+  const now = Date.now();
+  console.log('# mackerel-agent-plugin');
+  console.log(JSON.stringify(METRICS_DEFINITION));
+  console.log(`odango.humidity.my_room\t${humidity}\t${now}`);
+  console.log(`odango.temperature.my_room\t${temperature}\t${now}`);
+}
+
